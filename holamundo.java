becb3fc6523f383a007800ff69d5977ec1a3f653
Camila-Negron <113263409+Camila-Negron@users.noreply.github.com>
2022-09-10 17:56:52 -0400

    Create holamundo.java

---
+++ holamundo.java
@@ -0,0 +1,13 @@
+/**
+ * Hola mundo en Java
+ * 
+ * @author Camila Negron
+*/
+ clase  pública HolaMundo {
+  public  static  void  main ( String [] args ) {
+    sistema _ fuera _ println ( "Hola mundo" );
+    String  naranja = "\033[33m" ;
+    Cadena  azul = "\033[34m" ;
+    sistema _ fuera _ println ( naranja + "Hola" + azul + "mundo" );
+  }
+}
